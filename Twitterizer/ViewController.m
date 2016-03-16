82e6ca99e87578fc4f7028bd24c119e30bbb912f
Kyle <Ky@Kyles-MacBook-Pro.local>
2016-03-16 15:24:18 -0500

    3rd try

--- Twitterizer/ViewController.m
+++ Twitterizer/ViewController.m
@@ -9,7 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController () <UITextViewDelegate>
-@property (weak, nonatomic) IBOutlet UITextView *uiTextView;
+
+@property (weak, nonatomic) IBOutlet UITextView *textView;
+@property (weak, nonatomic) IBOutlet UILabel *label;
+
 
 
 @end
@@ -22,9 +25,32 @@
     [super viewDidLoad];
     
     
+    NSString *words = self.textView.text;
+    NSUInteger count = words.length;
+    self.label.text = @"Count: %i", count;
+    
 }
 
+- (IBAction)twitterizerButton:(UIButton *)sender {
+    NSMutableString *twitterize = [[NSMutableString alloc] initWithString: self.textView.text];
+    NSMutableString *consonant = [[NSMutableString alloc] init];
+    
+    NSCharacterSet *vowel = [NSCharacterSet characterSetWithCharactersInString:@"aeiouàèìòùáéíóúAEIOUÀÈÌÒÙÁÉÍÓÚ"];
+    
+    NSRange r;
+    
+    for (int i=0; i < [twitterize length]; i++) {
+        
+        r = [twitterize rangeOfCharacterFromSet:vowel];
+        
+        if(![vowel characterIsMember:[twitterize characterAtIndex:i]])
+            [consonant appendFormat:@"%C",[twitterize characterAtIndex:i]];
+        
+    }
+        self.textView.text = consonant;
+    
 
+}
 
 
 
